6ddb4ff11e124a7537cddaca739d6c17c9cc58cc
jann <jannandrea.f@gmail.com>
2023-01-04 14:12:52 +0100

    fehlercase abgedeckt

--- web/script.js
+++ web/script.js
@@ -4,3 +4,7 @@ let url = "https://thecocktaildb.com/api/json/v1/1/search.php?s=";
 
 let getInfo = () => {
   let userInp = document.getElementById("user-inp").value;
+
+  if (userInp.length == 0) {
+    result.innerHTML = `<h3 class="msg">Das Input Feld darf nicht leer sein!</h3>`;
+  } 
