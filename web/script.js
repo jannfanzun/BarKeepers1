a058425d6d5dd905df9bcf7fdcd683a5886b863f
jann <jannandrea.f@gmail.com>
2023-01-04 15:17:37 +0100

    fetch hinzugefügt damit ein fehlercase abgedeckt

--- web/script.js
+++ web/script.js
@@ -8,14 +8,23 @@ const getInfo = () => {
     result.innerHTML = `<h3 class="msg">Das Input Feld darf nicht leer sein!</h3>`;
     return;
   }
-}
+
+  fetch(url + userInput.value)
+  .then(response => response.json())
+  .then(data => {
+    userInput.value = "";
+    const drink = data.drinks[0];
+    if (!drink) {
+      result.innerHTML = `<h3 class="msg">Bitte geben Sie einen gültigen Namen ein!</h3>`;
+      return;
+    }
 
 let ingredients = "";
 for (let i = 1; i <= 18; i++) {
   if (drink[`strIngredient${i}`]) {
     ingredients += `<li>${drink[`strMeasure${i}`]} ${drink[`strIngredient${i}`]}</li>`;
   }
-}
+
 
 window.addEventListener("load", getInfo);
 searchBtn.addEventListener("click", getInfo);
