cece1616623f9b47684d3fd8c0a0b172aa976cac
jann <jannandrea.f@gmail.com>
2023-01-04 14:11:56 +0100

    userinput wird kontrolliert

--- web/script.js
+++ web/script.js
@@ -2,3 +2,5 @@ let result = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
 let url = "https://thecocktaildb.com/api/json/v1/1/search.php?s=";
 
+let getInfo = () => {
+  let userInp = document.getElementById("user-inp").value;
